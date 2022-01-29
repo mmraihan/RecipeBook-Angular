e3b9c92fd8553e4c781d317164fcadee8895e556
mmraihan <mm_raihan@live.com>
2022-01-29 22:09:08 +0600

    Add Content to the Recipes Components

--- RecipeBook/src/app/recipes/recipes.component.ts
+++ RecipeBook/src/app/recipes/recipes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('A test recipe','This is Description',`https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg`)
+  ]
 
   constructor() { }
 
