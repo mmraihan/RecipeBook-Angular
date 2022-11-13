f200eacf706bbc5b246487fc2eb7f64f93e1bb53
mmraihan <mm_raihan@live.com>
2022-11-13 20:35:36 +0600

    Custom Directive for drop down.

---
+++ RecipeBook/src/app/shared/adddropdown.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AdddropdownDirective } from './adddropdown.directive';
+
+describe('AdddropdownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AdddropdownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
