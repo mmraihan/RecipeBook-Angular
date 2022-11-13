550487a91eba8c5ad735a9ea4c69634f9219b7bb
mmraihan <mm_raihan@live.com>
2022-11-13 14:49:50 +0600

    Use ViewChild and custom event binding.

--- RecipeBook/src/app/shopping-list/shopping-list.component.ts
+++ RecipeBook/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
-import { Ingredient } from '../shared/ingredient.model';
+
 
 @Component({
   selector: 'app-shopping-list',
@@ -15,6 +16,10 @@ export class ShoppingListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addOngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+    console.log(ingredient.name)
   }
 
 }
