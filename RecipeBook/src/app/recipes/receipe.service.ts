c86fe192342d1dc69efc6d179aefec54df7c2fb5
mmraihan <mm_raihan@live.com>
2022-11-18 21:47:10 +0600

    Adding Editing Routes.

--- RecipeBook/src/app/recipes/receipe.service.ts
+++ RecipeBook/src/app/recipes/receipe.service.ts
@@ -6,12 +6,12 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class ReceipeService{
    private recipes: Recipe[] =[
-        new Recipe('A test recipe','This is Description',`https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg`,
+        new Recipe('A test recipe','This is DescriptionThis is Description',`https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg`,
          [
           new Ingredient('Oil',2),
           new Ingredient('Sugar', 3)
          ]),
-        new Recipe('A test 2 recipe','This is Description 2',`https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
+        new Recipe('A test 2 recipe','This is Description 2 This is Description',`https://images.pexels.com/photos/291528/pexels-photo-291528.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1`,
         [
           new Ingredient('Pizza',4),
           new Ingredient('Sauce', 10)
